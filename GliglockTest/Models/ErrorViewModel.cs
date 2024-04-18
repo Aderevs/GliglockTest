7ad6a114548dc05c4266269353df1307e114e182
Aderevs <aderevs@gmail.com>
2024-04-18 16:46:03 +0300

    Add project files.

---
+++ GliglockTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GliglockTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
